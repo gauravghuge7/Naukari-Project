d54caf97ed5ee21f48564ba585492adef79700f1
gaurav ashok ghuge <guduughuge7@gmail.com>
2025-01-25 22:39:25 +0530

    update te path url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,29 +6,18 @@ import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios';
 import { Auth0Provider } from '@auth0/auth0-react';
-import {io} from "socket.io-client";
+// import {io} from "socket.io-client";
 import { store } from './Redux/Store/Store.ts';
 import { Provider } from 'react-redux';
 
-export const  socket = io("http://localhost:5000/")
-
-console.log("Connection Successful Sockets", io)
 
 const url = import.meta.env.VITE_SERVER_URL;
 
-axios.create({
-    // baseURL: 'https://naukari-backend-production.up.railway.app/',
-    baseURL: 'http://localhost:5000/',
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',      
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Request-Method, Access-Control-Request-Headers',  
-        'Access-Control-Allow-Credentials': 'true'
-    },
-    withCredentials: true
 
-})
+axios.defaults.baseURL = url;
+axios.defaults.withCredentials = true;
+
+
 
 
 createRoot(document.getElementById('root')!).render(
